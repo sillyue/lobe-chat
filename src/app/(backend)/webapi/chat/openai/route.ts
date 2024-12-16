00d2087fffcec063264a16fd6096e3252f4f4d7a
sillyue <165244763+sillyue@users.noreply.github.com>
2024-12-16 10:23:08 +0800

    Update route.ts

--- src/app/(backend)/webapi/chat/openai/route.ts
+++ src/app/(backend)/webapi/chat/openai/route.ts
@@ -3,23 +3,18 @@ import { POST as UniverseRoute } from '../[provider]/route';
 export const runtime = 'edge';
 
 export const preferredRegion = [
-  'arn1',
-  'bom1',
-  'cdg1',
-  'cle1',
-  'cpt1',
-  'dub1',
-  'fra1',
-  'gru1',
-  'hnd1',
-  'iad1',
   'icn1',
-  'kix1',
-  'lhr1',
-  'pdx1',
-  'sfo1',
   'sin1',
+  'hnd1',
+  'kix1',
+  'bom1',
+  'cpt1',
+  'pdx1',
+  'cle1',
   'syd1',
+  'iad1',
+  'sfo1',
+  'gru1',
 ];
 
 export const POST = async (req: Request) =>
